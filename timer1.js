8f4f600923f54062739804441882f2b8a1be8169
Dexyod <dexyod43@gmail.com>
2020-07-28 17:17:59 +0000

    finished timer1.js

--- timer1.js
+++ timer1.js
@@ -1,3 +1,15 @@
 let args = process.argv.slice(2);
+const timer = (arr) => {
+  for (let num of arr) {
+    num = Number(num);
+    if (!num || typeof num !== "number" || num < 0) {
+      console.log("Invalid arguments");
+      process.exit(1);
+    }
+    setTimeout(() => {
+      process.stdout.write(".");
+    }, num * 1000);
+  }
+};
 
-const timer = (args) => {};
+timer(args);
